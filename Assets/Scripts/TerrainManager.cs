17da9d13ea597bf1519310cf43afdbf6bcdf3470
Dom Ryan <dommomo@gmail.com>
2017-12-18 15:19:30 +1030

    added infinite terrain with 'reframing'

--- Assets/Scripts/TerrainManager.cs
+++ Assets/Scripts/TerrainManager.cs
@@ -8,25 +8,51 @@ public class TerrainManager : MonoBehaviour {
     public int HorizontalTiles = 25;
     public int VerticalTiles = 25;
     public int Key = 1;
+    public Transform player;
+    public float maxDistanceFromPlayer = 7;
+
+    private SpriteRenderer[,] _renderers;
 
 	// Use this for initialization
 	void Start ()
     {
         var offset = new Vector3(0 - HorizontalTiles / 2, 0 - VerticalTiles / 2, 0);
-
+        _renderers = new SpriteRenderer[HorizontalTiles, VerticalTiles];
         for (int x = 0; x < HorizontalTiles; x++)
         {
             for (int y = 0; y < VerticalTiles; y++)
             {
                 var tile = new GameObject();
                 tile.transform.position = new Vector3(x, y, 0) + offset;
-                var spriteRenderer = tile.AddComponent<SpriteRenderer>();
-                spriteRenderer.sprite = SelectRandomSprite(x, y);
+                _renderers[x, y] = tile.AddComponent<SpriteRenderer>();
                 tile.name = "Terrain " + tile.transform.position;
                 tile.transform.parent = transform;
             }
         }
+
+        RedrawMap();
 	}
+
+    void Update()
+    {
+        if(maxDistanceFromPlayer < Vector3.Distance(player.position, transform.position))
+        {
+            RedrawMap();
+        }
+    }
+
+    void RedrawMap()
+    {
+        transform.position = new Vector3((int)player.position.x, (int)player.position.y, player.position.z);
+        for (int x = 0; x < HorizontalTiles; x++)
+        {
+            for (int y = 0; y < VerticalTiles; y++)
+            {
+                var spriteRenderer = _renderers[x, y];
+                spriteRenderer.sprite = SelectRandomSprite((int)transform.position.x + x, (int)transform.position.y + y);
+            }
+        }
+    }
 
     public Sprite SelectRandomSprite(int x, int y)
     {
