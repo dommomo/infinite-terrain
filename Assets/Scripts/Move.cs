1b4bbe33202090c8464f9ce7a36ec91e1ae80d9a
Dom Ryan <dommomo@gmail.com>
2017-12-18 14:56:18 +1030

    added camera movement

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 1;
+    public float fastSpeed = 3;
+    public KeyCode EnableFastSpeed = KeyCode.LeftShift;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var currentSpeed = speed;
+        if (Input.GetKey(EnableFastSpeed)) currentSpeed = fastSpeed;
+        var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.Translate(movement * currentSpeed * Time.deltaTime);
+	}
+}
