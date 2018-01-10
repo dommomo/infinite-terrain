018a4bca8000aae87a54af7ae060774f0e145080
Dom Ryan <dommomo@gmail.com>
2018-01-10 14:42:28 +1030

    added character graphics with movement

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour {
     public float speed = 1;
     public float fastSpeed = 3;
     public KeyCode EnableFastSpeed = KeyCode.LeftShift;
+    public Transform turnWithMovement;
 
 	// Use this for initialization
 	void Start () {
@@ -19,5 +20,10 @@ public class Move : MonoBehaviour {
         if (Input.GetKey(EnableFastSpeed)) currentSpeed = fastSpeed;
         var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.Translate(movement * currentSpeed * Time.deltaTime);
-	}
+
+        if (movement.x > 0.1f || movement.x < -0.1f || movement.y > 0.1f || movement.y < -0.1f)
+        { 
+            turnWithMovement.rotation = Quaternion.LookRotation(Vector3.back, movement.normalized);
+        }
+    }
 }
